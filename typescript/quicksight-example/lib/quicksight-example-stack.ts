380237f399f30f0de6297cea708a420754401ad7
Sundeep Ghotra <sundeep.ghotra@maibornwolff.de>
2024-09-16 11:56:50 +0200

    Trying to create a datasource and data set

--- typescript/quicksight-example/lib/quicksight-example-stack.ts
+++ typescript/quicksight-example/lib/quicksight-example-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import {Bucket} from "aws-cdk-lib/aws-s3";
 import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
 import {aws_s3} from "aws-cdk-lib";
+import {CfnAnalysis, CfnDataSet, CfnDataSource} from "aws-cdk-lib/aws-quicksight";
+import {CfnManagedPolicy, ManagedPolicy, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {readFileSync} from "node:fs";
 
 export class QuicksightExampleStack extends cdk.Stack {
   public static MANIFEST_KEY =
@@ -12,15 +15,46 @@ export class QuicksightExampleStack extends cdk.Stack {
     super(scope, id, props);
 
 
+    const bucketName = 'example-bucket';
+
     // Set up a bucket
-    const bucket = new aws_s3.Bucket(this, 'example-bucket', {
+    const bucket = new aws_s3.Bucket(this, bucketName, {
       accessControl: aws_s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
       encryption: aws_s3.BucketEncryption.S3_MANAGED,
       blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL
     });
 
+    const quicksightservicerole = new Role(
+      this, "aws-quicksight-service-role-v0",
+      {
+        roleName: "aws-quicksight-service-role-v0",
+        assumedBy: new ServicePrincipal('quicksight.amazonaws.com'),
+        description: 'quicksight role',
+      }
+    );
+
+    const S3ReadOnly = ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "s3quicksightPolicy",
+      "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    );
+
+    quicksightservicerole.addManagedPolicy(S3ReadOnly);
+
+    const qs_import_mode = "SPICE";
+
     this.deployManifests(bucket);
+    this.createManagedPolicyForQuicksight('quickSightPolicy', 'quickSightS3Policy', bucketName, [quicksightservicerole.roleName]);
+    const qs_s3_datasource_name = "s3datasourceexample";
+    const qs_s3_datasource = this.createDataSourceS3Type('S3DataSource', qs_s3_datasource_name, bucketName, QuicksightExampleStack.MANIFEST_KEY);
+    const physicalColumns = readFileSync('physical-columns.json', 'utf-8');
+    const physicalColumnsJson = JSON.parse(physicalColumns);
+
+    const physical_table_columns = physicalColumnsJson["Internal"];
+    const qs_s3_dataset_physical_tables_properties : CfnDataSet.PhysicalTableProperty = this.createS3PhysicalTableProperties(qs_s3_datasource.attrArn, physical_table_columns);
+    const qs_dataset_created = this.createDataset('ExtractTelemetryFinal', "s3-extract-telemetry", qs_import_mode, {[qs_s3_datasource_name]: qs_s3_dataset_physical_tables_properties});
   }
+
   public deployManifests(bucket: Bucket) {
     const manifest = QuicksightExampleStack.createS3Manifest(
       bucket.bucketName
@@ -51,4 +85,114 @@ export class QuicksightExampleStack extends cdk.Stack {
       },
     };
   }
+
+  createManagedPolicyForQuicksight(idManagedPolicy: string, namePolicy: string, bucketName: string, roles_quicksight: string[]): CfnManagedPolicy {
+    return new CfnManagedPolicy(
+      this,
+      idManagedPolicy,
+      {
+        managedPolicyName: namePolicy,
+        policyDocument: {
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Action": ["s3:ListAllMyBuckets"],
+              "Resource": ["arn:aws:s3:::*"]
+            },
+            {
+              "Effect": "Allow",
+              "Action": ["s3:ListBucket"],
+              "Resource": [
+                `arn:aws:s3:::${bucketName}`
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "s3:GetObject",
+                "s3:List*"
+              ],
+              "Resource": [
+                `arn:aws:s3:::${bucketName}/*`
+              ]
+            }
+          ],
+          "Version": "2012-10-17"
+        },
+        roles: roles_quicksight
+      }
+    );
+  }
+
+  createDataSourceS3Type(idDataSource: string, nameSource: string, bucketName: string, manifestKey: string): CfnDataSource {
+    return new CfnDataSource(
+      this,
+      idDataSource,
+      {
+        awsAccountId: this.account,
+        dataSourceId: nameSource,
+        name: nameSource,
+        dataSourceParameters: {
+          s3Parameters: {
+            manifestFileLocation: {
+              bucket: bucketName,
+              key: manifestKey
+            }
+          }
+        },
+        type: 'S3',
+        sslProperties: {
+          disableSsl: false
+        }
+      }
+    )
+  }
+
+  createDataset(idDataset: string, datasetName: string, importMode: string, physical_table: Record<string, CfnDataSet.PhysicalTableProperty>): CfnDataSet {
+    return new CfnDataSet(
+      this,
+      idDataset,
+      {
+        awsAccountId: this.account,
+        physicalTableMap: physical_table,
+        name: datasetName,
+        dataSetId: datasetName,
+        importMode: importMode
+      }
+    );
+  }
+
+  createAnalysis(idAnalysis: string, nameAnalysis: string, templateSourceArn: string, datasetCreatedArn: string, datasetPlaceholder: string): CfnAnalysis {
+    return new CfnAnalysis(this, idAnalysis, {
+      awsAccountId: this.account,
+      name: nameAnalysis,
+      analysisId: nameAnalysis,
+      sourceEntity: {
+        sourceTemplate: {
+          arn: templateSourceArn,
+          dataSetReferences: [
+            {
+              dataSetArn: datasetCreatedArn,
+              dataSetPlaceholder: datasetPlaceholder
+            }
+          ]
+        }
+      },
+    });
+  }
+
+  createS3PhysicalTableProperties(arnDataSourceCreated: string, inputColumns: CfnDataSet.InputColumnProperty[]) : CfnDataSet.PhysicalTableProperty{
+    return {
+      s3Source: {
+        dataSourceArn: arnDataSourceCreated,
+        inputColumns: inputColumns,
+        uploadSettings: {
+          format: 'CSV',
+          delimiter: ',',
+          containsHeader: true
+        }
+      }
+    }
+  }
+
 }
